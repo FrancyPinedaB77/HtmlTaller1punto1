049c83c24e9ca5cd9df58b98f2f955b08037abd3
imadie <divmermarlav@gmail.com>
2016-08-22 02:51:44 -0500

    [Punto 2] descarga, filtrado y unido listo. Queda la busqueda

--- preeliminar/worker/punto2/descargaParalela.sh
+++ preeliminar/worker/punto2/descargaParalela.sh
@@ -1,8 +1,22 @@
-PARAMS=''
+#!/bin/bash
+
+PARAMS_DESCARGA=''
+FEEDS=''
 n=0
 for i in $(cat fuentesRSS.csv)
 do c=$(echo $i -O feed$n.xml)
+FEEDS+=feed$n.xml' '
 n=$(($n+1))
-PARAMS+=$c' '
+PARAMS_DESCARGA+=$c' '
 done
-echo $PARAMS | xargs -n 3 -P 8 wget -q 
+mkdir tmp; cd tmp
+echo $PARAMS_DESCARGA | xargs -n 3 -P 4 wget -q
+echo $FEEDS | xargs -n 1 -P 4 python ../limpiezaFeeds.py
+FEED_FINAL='db_feed.xml'
+echo "<resultados>" > $FEED_FINAL
+cat feed* >> $FEED_FINAL
+echo "</resultados>" >> $FEED_FINAL
+rm feed*
+mv $FEED_FINAL ../
+cd ..
+rm -rf tmp
