2ff7b476f7acd754a105aa3802583ab43901db3b
Christian Poveda <cn.poveda.ruiz@gmail.com>
2016-08-22 18:49:33 -0500

    working version

--- servidor/noticias/views.py
+++ servidor/noticias/views.py
@@ -3,15 +3,15 @@ from . import models
 from .forms import SearchForm
 
 # Create your views here.
+
 def index(request):
-    newsReader = models.NewsReader('data_full.xml')
+    newsReader = models.NewsReader()
     regexlist = [] # Lista de noticias filtradas con regex
     xquerylist = [] # Lista de noticias filtradas con xquery
     if request.method == 'POST': 
         form = SearchForm(request.POST)
         if form.is_valid():
-            regexlist = newsReader.filter(form.cleaned_data['search'], form.cleaned_data['kind'])
-            xquerylist = regexlist
+            xquerylist = newsReader.filter_xquery(form.cleaned_data['search'], form.cleaned_data['kind'])
     else:
         form = SearchForm()
     context = {'newslist': newsReader.news, 'regexlist': regexlist, 'xquerylist': xquerylist , 'form': form}
