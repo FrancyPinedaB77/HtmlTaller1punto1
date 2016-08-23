ef8f8a1aea0379b0a189caf2d643b36c18feb432
Christian Poveda <cn.poveda.ruiz@gmail.com>
2016-08-23 15:22:52 -0500

    spidy tiene su primera pata

--- worker/punto1/spidy.py
+++ worker/punto1/spidy.py
@@ -1,1 +1,11 @@
+import re
 from urllib import request
+
+with request.urlopen('http://uniandes.edu.co/institucional/facultades/facultades') as response:
+   root = response.read().decode()
+
+x = re.findall('<li><a\s+href="https?://([^"]+uniandes\.edu\.co)[^"]*"\s+[^>]*>([^<]*(Facultad|Departamento|Centro|Escuela)[^<]*)</a></li>', root)
+x = list(set([i[:2] for i in x]))
+x.sort(key=lambda i: i[1])
+for i in x:
+    print(i)
