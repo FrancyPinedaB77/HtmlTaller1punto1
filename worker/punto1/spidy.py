c6c189390e630c721ae2d774499d7a55d4c3209e
Christian Poveda <cn.poveda.ruiz@gmail.com>
2016-08-23 19:49:37 -0500

    probando links a eventos

--- worker/punto1/spidy.py
+++ worker/punto1/spidy.py
@@ -4,8 +4,27 @@ from urllib import request
 with request.urlopen('http://uniandes.edu.co/institucional/facultades/facultades') as response:
    root = response.read().decode()
 
-x = re.findall('<li><a\s+href="https?://([^"]+uniandes\.edu\.co)[^"]*"\s+[^>]*>([^<]*(Facultad|Departamento|Centro|Escuela)[^<]*)</a></li>', root)
-x = list(set([i[:2] for i in x]))
-x.sort(key=lambda i: i[1])
-for i in x:
-    print(i)
+units = re.findall('<li><a\s+href="https?://([^"]+uniandes\.edu\.co)[^"]*"\s+[^>]*>([^<]*(Facultad|Centro|Escuela|Departamento)[^<]*)</a></li>', root)
+units = list(set([i[:2] for i in units]))
+units.sort(key=lambda i: i[1])
+
+for (url, name) in units:
+    url = 'http://{}'.format(url)
+    print('Searching events site for', name, 'at', url)
+    unit_site = None
+    try:
+        with request.urlopen(url) as response:
+            unit_site = response.read().decode()
+    except:
+        print('Request failed!!!')
+
+    if unit_site:
+        events_links = re.findall('href=["\']([^\'"]*([Ee]ventos|icagenda|icalrepeat)[^\'"]*)["\']', unit_site)
+        if len(events_links) == 0:
+            print(' ','Didn\'t found any link to an events page!')
+        else:
+            print(' ','Links to events found:')
+            events_links.sort(key=lambda i: len(i[0]))
+            for i in events_links:
+                print('   ', i[0])
+    print('')
