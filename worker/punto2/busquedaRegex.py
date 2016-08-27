1e2e346bbc2bca0af62d64cb1d33447c69d17e33
imadie <divmermarlav@gmail.com>
2016-08-26 22:15:05 -0500

    [Punto 2] listas las busquedas por REGEX

--- worker/punto2/busquedaRegex.py
+++ worker/punto2/busquedaRegex.py
@@ -9,9 +9,10 @@ if numargs < 3:
     print('insuficientes argumentos')
     sys.exit(1)
 
+flag = 0
 # opcion para que sea ignorecase = True
 if '-i' ==  sys.argv[1]:
-    ignoreCase = True
+    flag = re.I
     campo = sys.argv[2]
     if numargs >= 3:
         palabras = sys.argv[3:]
@@ -19,7 +20,6 @@ if '-i' ==  sys.argv[1]:
         print('insuficientes argumentos')
         sys.exit(1)
 else:
-    ignoreCase = False
     campo = sys.argv[1]
     palabras = sys.argv[2:]
 
@@ -30,16 +30,16 @@ qT0 = r"<item><id>(\d+)</id><title>"
 qT1 = r"</t"
 
 # Cadenas iniciales y finales para Descripcion (D0,D1)
-qD0 = r"<item><id>(\d+)</id>(<[^>]+>[^<]+</[^>]+>){3}<description>"
+qD0 = r"<item><id>(\d+)</id>(?:<[^>]+>[^<]+</[^>]+>){3}<description>"
 qD1 = r"</d"
 
 # Cadenas iniciales y finales para Categoria (C0,C1)
-qC0 = r"<item><id>(\d+)</id>(<[^>]+>[^<]+</[^>]+>){4}\
-      (<category>[^<]+</category>)*(<category>"
-qC1 = r"</category>)+(<category>[^<]+</category>)*"
+qC0 = r"<item><id>(\d+)</id>(?:<[^>]+>[^<]+</[^>]+>){4}\
+      (?:<category>[^<]+</category>)*(?:<category>"
+qC1 = r"</category>)+(?:<category>[^<]+</category>)*"
 
 # Juntar cada palabra con el operador "o"
-regexOr = c + "("
+regexOr = c + "(?:"
 for w in palabras[:-1]:
     regexOr += w + "|"
 regexOr+=palabras[-1] + ")" + c
@@ -48,11 +48,11 @@ regexOr+=palabras[-1] + ")" + c
 perm = list(permutations(palabras))
 regexAnd = ""
 for per in perm[:-1]:
-    regexAnd += "(" + c
+    regexAnd += "(?:" + c
     for w in list(per):
         regexAnd += str(w) + c 
     regexAnd += ")|"
-regexAnd += "(" + c
+regexAnd += "(?:" + c
 for w in list(perm[-1]):
     regexAnd += str(w) + c 
 regexAnd+=")"
@@ -75,4 +75,10 @@ else:
     sys.exit(1)
 
 feed = open('./db_feed.xml','r').read()
-print(re.findall(queryOr,feed))
+results = re.findall(queryAnd,feed,flags=flag)+\
+          re.findall(queryOr,feed,flags=flag)
+uniq = []
+for item in results:
+    if item not in uniq:
+        uniq.append(item)
+print("\n".join(uniq))
